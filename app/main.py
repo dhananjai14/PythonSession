c63b2f52a8e731ddbe065672b86074668e767b47
Dhananjai Singh <dhananjai.eee@gmail.com>
2025-06-26 08:07:44 +0530

    26-06-2025

--- app/main.py
+++ app/main.py
@@ -1,17 +1,28 @@
 # We are creating an web application 
 
-from flask import Flask
+from flask import Flask, session
 from app.test import Animal
-
 
 app = Flask(__name__)
 
-@app.route("/")
+variable1 = ""
+
+@app.route("/test", methods = ['GET'])
 def my_first_web_app():
     animal = Animal()
+    global variable1
+    variable1 = "Hello World"
     return animal.eat() 
+
+# Take stock code from user
+@app.route("/stock/<stock_name>", methods = ["GET"])
+def stock(stock_name):
+    print(f"User gave stock {stock_name}")
+    global variable1
+    variable1 = "Hello World from stock"
+    print(variable1)
+    return "Hi You gave the following stock Name: " + stock_name
+
 
 if __name__ == "__main__":
     app.run()
-
-
